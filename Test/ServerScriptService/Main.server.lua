e5822c7ce1563a874f72dc485ab81d2e484a08e4
Pokesquirrel <39865970+Pokesquirrel@users.noreply.github.com>
2026-04-18 17:45:38 -0700

    Create Main.server.lua

---
+++ Test/ServerScriptService/Main.server.lua
@@ -0,0 +1,13 @@
+local AIManager = require(script.AI.AIManager)
+local Threat = require(script.AI.Systems.ThreatSystem)
+local Metrics = require(script.AI.Systems.Metrics)
+local Director = require(script.AI.DirectorAI)
+
+Metrics:Start()
+Director:Init()
+AIManager:Init()
+
+game:GetService("RunService").Heartbeat:Connect(function(dt)
+	Threat:Update(dt)
+	Metrics:Tick(dt)
+end)
